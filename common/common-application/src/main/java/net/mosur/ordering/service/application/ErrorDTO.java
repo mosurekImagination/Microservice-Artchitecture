00ae04f1e9ff90984f14176c8004230b8d18fdfc
Tomasz Mosur <tomasz.mosur@sixdx.com>
2023-02-13 23:48:34 +0100

    Controllers

---
+++ common/common-application/src/main/java/net/mosur/ordering/service/application/ErrorDTO.java
@@ -0,0 +1,13 @@
+package net.mosur.ordering.service.application;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ErrorDTO {
+    private final String code;
+    private final String message;
+}
---
+++ order-service/order-application/src/main/java/net/mosur/ordering/service/application/exception/handler/OrderGlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package net.mosur.ordering.service.application.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import net.mosur.ordering.order.service.domain.exception.OrderDomainException;
+import net.mosur.ordering.order.service.domain.exception.OrderNotFoundException;
+import net.mosur.ordering.service.application.ErrorDTO;
+import net.mosur.ordering.service.application.GlobalExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice
+public class OrderGlobalExceptionHandler extends GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {OrderDomainException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleException(OrderDomainException orderDomainException){
+        log.error(orderDomainException.getMessage(), orderDomainException);
+        return ErrorDTO.builder()
+                .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(orderDomainException.getMessage())
+                .build();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = {OrderNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorDTO handleException(OrderNotFoundException orderNotFoundException){
+        log.error(orderNotFoundException.getMessage(), orderNotFoundException);
+        return ErrorDTO.builder()
+                .code(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .message(orderNotFoundException.getMessage())
+                .build();
+    }
+}
